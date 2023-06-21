76befa479bd4fbca721fecd46e69c1196c5e307b
keksShmeks1 <nazaresha2000@yandex.ru>
2023-06-21 21:19:34 +0300

    Working version 60 percent accuracy

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from solver import PuzleSolver
-from PIL import Image
+from trueSolver import get_points
 
 # Initialize ChromeDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -29,6 +28,7 @@ phone_input = driver.find_element(By.XPATH, "//input[@id='linkphone']")
 email_input = driver.find_element(By.XPATH, "//input[@id='mail']")
 number_input = driver.find_element(By.XPATH, "//input[@id='applyid1']")
 
+
 fio_input.send_keys(fio)
 phone_input.send_keys(phone)
 email_input.send_keys(email)
@@ -47,15 +47,19 @@ wait = WebDriverWait(driver, 10)
 iframe1 = wait.until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[id='tcaptcha_iframe_dy']"))
 )
+
 driver.switch_to.frame(iframe1)
 print("switched to the frame with captcha")
 
-time.sleep(10)
-network_requests = driver.execute_script("return window.performance.getEntries()")
+
+time.sleep(3)
+
 
 background_imgage = None
 piece_image = None
 
+
+network_requests = driver.execute_script("return window.performance.getEntries()")
 for request in network_requests:
     if "cap_union_new_getcapbysig?img_index=1" in request["name"]:
         print("FOUND background")
@@ -65,44 +69,38 @@ for request in network_requests:
         piece_image = request["name"]
         break
 
-print("found nothing")
 
-distance = 20
-if background_imgage and piece_image:
-    response_back = requests.get(background_imgage)
-    response_piece = requests.get(piece_image)
-    with open("background.png", "wb") as file:
-        file.write(response_back.content)
-    with open("piece.png", "wb") as file:
-        file.write(response_piece.content)
-    piece_img = Image.open("piece.png")
-    piece_img_cropped = piece_img.crop((155, 504, 245, 594))
-    piece_img_cropped.save("piece_cropped.png", quality=95)
-    solver = PuzleSolver("background.png", "piece_cropped.png")
-    distance = solver.get_position()
-    print(solver.get_position())
+distance = 0
+try:
+    if background_imgage and piece_image:
+        response_back = requests.get(background_imgage)
+        response_piece = requests.get(piece_image)
+        with open("background.png", "wb") as file:
+            file.write(response_back.content)
+        distance = int(get_points("background.png") / 2.6) + 5
+        # Дополнительный код, использующий значение distance
+    else:
+        raise ValueError("Отсутствуют ссылки на изображения")
+except Exception as e:
+    print("Ошибка при решении капчи:", str(e))
+    print("Капча не решаема")
 
 
-# Wait for the slider to load inside the second iframe
+print(type(distance))
+
+
 slider = wait.until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "div[aria-label='Проведите, чтобы собрать пазл']")
-    )
+    EC.presence_of_element_located((By.CSS_SELECTOR, "img[alt='slider']"))
 )
-
-# Calculate the distance to drag the slider
-
-
-# container_width = slider.size["width"]
-slider_width = slider.size["width"]
 
 # Perform the sliding action
 actions = webdriver.ActionChains(driver)
-actions.click_and_hold(slider).move_by_offset(distance, 0).release().perform()
-
-
+actions.click_and_hold(slider).perform()
+actions.move_by_offset(distance,0).perform()
+actions.release(slider).perform()
 # Wait for verification or further actions
-time.sleep(5)
+
+time.sleep(10)
 
 # Close the browser
 driver.quit()
---
+++ trueSolver.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+
+def get_points(background_path):
+    # Загрузка изображения
+    image = cv2.imread(background_path)
+
+    # Преобразование изображения в градации серого
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Применение фильтра Гаусса для сглаживания изображения
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Применение адаптивной пороговой обработки для выделения контура
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+
+    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Фильтрация контуров по цвету и площади
+    filtered_contours = []
+    for i, contour in enumerate(contours):
+        # Проверка иерархии контура
+        if hierarchy[0][i][3] == -1:
+            # Вычисление прямоугольной области, охватывающей контур
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # Проверка площади и размеров
+            if w * h >= 70 * 70 and w <= 100 and h <= 100:
+                filtered_contours.append(contour)
+
+    # Нахождение контура с наибольшей площадью
+    largest_contour = max(filtered_contours, key=cv2.contourArea)
+
+    # Вычисление координат и центра контура
+    x, y, w, h = cv2.boundingRect(largest_contour)
+    center_x = x + w // 2
+    center_y = y + h // 2
+
+    # Вывод координат центра
+    print("Координаты центра пазла: ({}, {})".format(center_x, center_y))
+    mask = np.zeros_like(gray)
+
+    # Заливка контуров на маске
+    cv2.drawContours(mask, filtered_contours, -1, (255), cv2.FILLED)
+
+    # Применение маски к исходному изображению
+    result = cv2.bitwise_and(image, image, mask=mask)
+
+    # Отображение результата
+    # cv2.imshow('Captcha', result)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return center_x
